6f66d35be1084474d06826593c82cfce87ea08fe
Natham Fernandes <natham.fr@gmail.com>
2022-01-13 21:39:22 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -6,11 +6,11 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_score")
-
 public class Score {
 
 	@EmbeddedId
 	private ScorePK id = new ScorePK();
+	
 	private Double value;
 	
 	public Score() {
